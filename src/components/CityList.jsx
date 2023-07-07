ea87c7314b9b2165ea5d15ef0f8073da72bbb4f9
Heechem <hichempeter@gmail.com>
2023-07-07 08:18:07 -0600

    Rendering city data

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
 
-const CityList = () => {
-  return <ul className={styles.cityList}>CityList</ul>;
+const CityList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem
+          city={city}
+          key={city.id}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default CityList;
