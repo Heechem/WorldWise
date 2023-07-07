3d40afa5866474fcaf699a9ad55bb59bb9ea8700
Heechem <hichempeter@gmail.com>
2023-07-07 09:36:55 -0600

    Adding rendering country logic

--- src/components/CoutriesList.jsx
+++ src/components/CoutriesList.jsx
@@ -2,8 +2,10 @@ import CityItem from "./CityItem";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
 
 const CountriesList = ({ cities, isLoading }) => {
+  console.log(cities);
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
@@ -12,13 +14,16 @@ const CountriesList = ({ cities, isLoading }) => {
         message={"Add your first city by clicking on a city on the map"}
       />
     );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
   return (
     <ul className={styles.countryList}>
-      {cities.map((city) => (
-        <CityItem
-          city={city}
-          key={city.id}
-        />
+      {countries.map((country) => (
+        <CountryItem country={country} />
       ))}
     </ul>
   );
