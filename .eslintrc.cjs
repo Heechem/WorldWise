0becc40e838e75103b66d14e3aa7bc0ce306cc66
Heechem <hichempeter@gmail.com>
2023-07-10 07:50:15 -0600

    Adding Eslint Config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -12,5 +12,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'off',
   },
 };
