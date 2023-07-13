634da8ae23ff3a5c3da6c165b277f4d3626f3e0e
Heechem <hichempeter@gmail.com>
2023-07-13 08:37:45 -0600

    Adding the user login display

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
   }
   useEffect(() => {
     console.log(123);
-    if (isAuthenticated) navigate('/app');
+    if (isAuthenticated) navigate('/app', { replace: true });
   }, [isAuthenticated, navigate]);
 
   return (
