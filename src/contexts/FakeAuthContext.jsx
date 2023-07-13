0da2dc55723e945b7334b5ca970e20882899adce
Heechem <hichempeter@gmail.com>
2023-07-13 06:52:11 -0600

    Adding fakeAuth Context

---
+++ src/contexts/FakeAuthContext.jsx
@@ -0,0 +1,36 @@
+const { createContext, useContext, useReducer } = require('react');
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'login':
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+  }
+}
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated, dispatch }] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {}
+  function logout() {}
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error('Context was used outside of the provider ');
+}
